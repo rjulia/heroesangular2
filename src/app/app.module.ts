0460c6edbf47d53e463408ae184e9b7561820c1b
nitroclik <reimon1972@gmail.com>
2017-04-23 21:54:09 +0200

    first app to push

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//routes
+import { APP_ROUTING } from "./app.routes";
+
+
+// services
+
+import { HeroesService } from "./services/heroes.service";
+
+//components
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from "../../services/heroes.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesServicies:HeroesService,
+              private _router:Router) {
+
+
+   }
+
+  ngOnInit() {
+    this.heroes = this._heroesServicies.getHerores();
+    // console.log(this.heroes);
+    
+
+  }
+  verHeroe( idx:number){
+    this._router.navigate(['/heroe',idx])
+    
+  }
+
+}
